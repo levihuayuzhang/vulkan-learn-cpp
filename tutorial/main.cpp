0d4b90b7de13b78e0f51f41cdd2ffb1783aaa830
Huayu Zhang <zhanghuayu.dev@gmail.com>
2023-01-08 02:25:37 +0000

    get available extensions

--- tutorial/main.cpp
+++ tutorial/main.cpp
@@ -6,6 +6,7 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <vector>
+#include <algorithm>
 
 class HelloTriangleApplication {
 public:
@@ -53,27 +54,43 @@ private:
         /* Since 1.3.216 Vulkan SDK, need add `VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR` to flags
          * and `VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME` to enabled extension name
          */
-        std::vector<const char*> requiredExtensions; // use vector handle extensions
+        std::vector<const char*> requiredExtensions; // use vector handle for extensions returned from glfw
         for (std::uint32_t i = 0; i < glfwExtensionCount; i++) {
             requiredExtensions.emplace_back(glfwExtensions[i]);
         }
+#ifdef __APPLE__
         requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME); // for newer macOS SDK
-
+#endif
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
+#ifdef __APPLE__
         createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR; // for newer macOS SDK
+#endif
         createInfo.enabledExtensionCount = (std::uint32_t) requiredExtensions.size();
         createInfo.ppEnabledExtensionNames = requiredExtensions.data();
         createInfo.enabledLayerCount =0;
 
+        // get available extension
+        std::vector<VkExtensionProperties> availableExtensions = getAvailableExtensions ();
+
+//todo: check extensions required by glfw is available in vulkan supported extension list
+//        std::vector<const char*> availableExtensionNames;
+//        for (std::uint32_t i = 0; i < availableExtensions.size(); i++) {
+//            availableExtensionNames.emplace_back(availableExtensions[i].extensionName);
+//        }
+//        bool allAvailable = checkExtensions( availableExtensionNames, requiredExtensions);
+//        if (allAvailable) {
+//            std::cout << "\n All Required Extensions are all available" << "\n";
+//        }
+
+        // creating instance
         VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
         if (result != VK_SUCCESS) {
             throw std::runtime_error("Failed to create instance...");
+        }else {
+            std::cout << "\nInstance Create Success!!!" << std::endl;
         }
-//        else if (result == VK_SUCCESS) {
-//            std::cout << "Instance create success at: "<< printf("%p", &instance) << std::endl;
-//        }
     }
 
     void mainLoop() {
@@ -83,10 +100,46 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
     }
+
+    // get available extension
+    std::vector<VkExtensionProperties> getAvailableExtensions (){
+        std::uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available Extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << "\n";
+        }
+
+        return extensions;
+    }
+
+//todo: check extensions required by glfw is available in vulkan supported extension list
+//    bool checkExtensions (std::vector<const char*> availableExtensions, std::vector<const char*> requiredExtensions) {
+////        std::sort(requiredExtensions.begin(), requiredExtensions.end());
+////        std::sort(availableExtensions.begin(), availableExtensions.end());
+////        return std::includes(availableExtensions.begin(), availableExtensions.end(),requiredExtensions.begin(), requiredExtensions.end());
+//
+//        bool allAvailable = false;
+//        for (std::uint32_t i = 0; i < requiredExtensions.size(); i++){
+//            allAvailable = allAvailable || false;
+//            for (std::uint32_t j = 0; j < availableExtensions.size(); j++){
+//                if (requiredExtensions[i] == availableExtensions[j]) {
+//                    allAvailable = true;
+//                    break;
+//                }
+//            }
+//        }
+//        return allAvailable;
+//    }
 };
 
 int main () {
